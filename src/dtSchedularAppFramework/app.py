21e14ae01c37740bfc42a445bed6a284f499391a
Digital-Thought <matthew@digital-thought.org>
2023-10-18 09:26:40 +1100

    Added option to disable job via config file "disabled" flag

--- src/dtSchedularAppFramework/app.py
+++ src/dtSchedularAppFramework/app.py
@@ -22,16 +22,17 @@ class AbstractSchedularApp(AbstractApp):
 
     def load_jobs(self):
         for job in Settings().get('jobs', []):
-            logging.info(f'Loading Job: {job["name"]}')
-            module = importlib.import_module(job["module"])
-            if 'module_settings' in job:
-                job_module: AbstractJob = module.Job(job_name=job["name"], **job["module_settings"])
-            else:
-                job_module: AbstractJob = module.Job(job_name=job["name"])
+            if 'disabled' not in job or job['disabled']:
+                logging.info(f'Loading Job: {job["name"]}')
+                module = importlib.import_module(job["module"])
+                if 'module_settings' in job:
+                    job_module: AbstractJob = module.Job(job_name=job["name"], **job["module_settings"])
+                else:
+                    job_module: AbstractJob = module.Job(job_name=job["name"])
 
-            job = self.scheduler.add_job(job_module.run, trigger=job['trigger'], id=job['name'], **job['schedule'])
-            self.jobs.append(job)
-            self.job_modules.append(job_module)
+                job = self.scheduler.add_job(job_module.run, trigger=job['trigger'], id=job['name'], **job['schedule'])
+                self.jobs.append(job)
+                self.job_modules.append(job_module)
 
     def main(self, args):
         self.load_jobs()
